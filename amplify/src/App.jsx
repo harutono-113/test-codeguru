df68c77b4d4725bfa432d399ec17589778bacebb
harutono-113 <harutono13@outlook.jp>
2026-09-09 08:23:04 +0900

    Initial commit

---
+++ amplify/src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="https://vitejs.dev/logo.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg" className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <p className="read-the-docs">
+        You have successfully created a React app and are ready for AWS Amplify!
+      </p>
+    </>
+  )
+}
+
+export default App
